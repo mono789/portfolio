35fc9c0254866abf45566848921c2b133ee27e9d
apoyodesarrollo6.ing <apoyodesarrollo6.ing@udea.edu.co>
2025-01-29 22:21:56 -0500

    portfolio updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,41 +105,152 @@ export default async function Home() {
       ],
       thumbnail: "/icons/modelo.jpg"
     },
+    {
+      title: "Flight Delays Prediction",
+      slug: "project-3",
+      description: "This project focuses on the analysis and prediction of flight delays using historical data from various airlines during 2015. The goal is to understand the factors that influence delays, such as the origin airport, the airline, and the interactions between origin and destination airports, to improve prediction accuracy and propose solutions to anticipate these issues.",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/IA_UdeA_FlightPredict",
+      dateRange: { start: "2024-09-08", end: "2024-11-10" },
+      tags: [
+        { name: "Python", icon: "/icons/python.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Docker", icon: "/icons/docker.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/delayed.jpg"
+    },
+    {
+      title: "Business Rules",
+      slug: "project-4",
+      description: "I created an application for a banking study service for credit granting, which will determine whether a client can receive a loan and the interest rate of the granted loan, provided that certain established conditions are met.",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/drools_rules",
+      dateRange: { start: "2024-10-01", end: "2024-05-11" },
+      tags: [
+        { name: "Java", icon: "/icons/java.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Drools", icon: "/icons/drools-seeklogo.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Spring", icon: "/icons/spring-3.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/drools.png"
+    },
+    {
+      title: "Real-time boarding gate",
+      slug: "project-5",
+      description: "I developed a messaging application to monitor real-time boarding gate information at an airport (GateBoard).",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/web-sockets",
+      dateRange: { start: "2024-11-01", end: "2024-12-10" },
+      tags: [
+        { name: "Java", icon: "/icons/java.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Maven", icon: "/icons/maven-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Node.js", icon: "/icons/node-js.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Spring", icon: "/icons/spring-3.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "React", icon: "/icons/react.png", slug: "frontend", categories: [{ title: "Frontend", slug: "frontend" }] },
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/web-socket.png"
+    },
+    {
+      title: "RESTful application",
+      slug: "project-6",
+      description: "Develop and deploy a RESTful application using Spring Boot with HATEOAS principles, containerizing it with Docker, integrating it with PostgreSQL, and managing its deployment in a Kubernetes cluster to ensure functionality, scalability, and efficiency.",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/rest-kubernetes",
+      dateRange: { start: "2024-11-01", end: "2024-12-10" },
+      tags: [
+        { name: "Java", icon: "/icons/java.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Postgres", icon: "/icons/postgres.png", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Spring", icon: "/icons/spring-3.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Minikube", icon: "/icons/minikube-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Kubernetes", icon: "/icons/kubernetes-icon.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Swagger", icon: "/icons/swagger-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Maven", icon: "/icons/maven-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/kubernetes.png"
+    },
+
+    {
+      title: "CI/CD Application",
+      slug: "project-7",
+      description: "A Java backend built with Spring Boot, designed to highlight a robust DevOps workflow. It includes a CI/CD pipeline using GitHub Actions, integrating automated unit tests, code quality analysis with SonarCloud, security scanning via Snyk, test coverage tracking with Coveralls, and deployment to AWS EC2 for cloud hosting.",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/CI_CD_Application",
+      dateRange: { start: "2024-11-01", end: "2024-12-10" },
+      tags: [
+        { name: "Java", icon: "/icons/java.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Docker", icon: "/icons/docker.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/actions.png"
+    },
+
+    {
+      title: "Grafana and Prometheus",
+      slug: "project-8",
+      description: "An API built with Spring Boot and GraphQL, featuring Docker for seamless containerization and deployment. It stands out for its integration of Prometheus and Grafana, offering real-time monitoring, insightful metrics, and dynamic dashboards for exceptional performance management.",
+      websiteUrl: "https://example.com/project-1",
+      githubUrl: "https://github.com/mono789/Grafana-Prometheus",
+      dateRange: { start: "2024-11-01", end: "2024-12-10" },
+      tags: [
+        { name: "Java", icon: "/icons/java.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Docker", icon: "/icons/docker.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Spring", icon: "/icons/spring-3.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Prometheus", icon: "/icons/prometheus-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+        { name: "Grafana", icon: "/icons/grafana-svgrepo-com.svg", slug: "backend", categories: [{ title: "Backend", slug: "backend" }] },
+      ],
+      content: [
+        "<p>.</p>"
+      ],
+      thumbnail: "/icons/grafana.png"
+    },
     
   ];
 
   const experiences = [
     {
-      organization: " Tutor in Machine Learning Technical Component",
-      title: "Sapiencia",
-      dateRange: { start: "September 2023", end: "November 2023" },
-      description: "I taught Python for Machine Learning to 30 students remotely, developing and managing both theoretical and practical content. My efforts led to successful student certifications and a comprehensive learning experience.",
-      content: "<p>.</p>",
+      organization: "Full-Stack Developer",
+      title: "University of Antioquia",
+      dateRange: { start: "August 2024", end: "" },
+      description: "Collaborated on optimizing multiple university platforms with Next.js and Spring Boot, enhancing user experience for over 10,000 monthly visitors. Upgraded outdated codebases, reducing system response times by 35% and integrating SEO improvements for better discoverability.",
+      content: "<p>.</p>", // Un solo string en lugar de un array
       slug: "experience-1"
-    },
-    {
-      organization: "Founder and CEO",
-      title: "Parchese Store",
-      dateRange: { start: "October 2017", end: "December 2023" },
-      description: "I made key decisions to meet sales goals and ensure financial control. I led a team of 5, managing tasks such as payroll and inventory. I launched a website with 2500 products, optimized inventory and shipping processes, and handled digital marketing and content development.",
-      content: "<p>.</p>", // Un solo string en lugar de un array
-      slug: "experience-2"
-    },
-    {
-      organization: "Lost Item Agent at Uber",
-      title: "Teleperformance",
-      dateRange: { start: "October 2017", end: "December 2023" },
-      description: "I helped Uber users recover lost items and was named Top Performer for exceeding KPIs. I developed skills in adaptability, learning, and communication while supporting various customer service areas.",
-      content: "<p>.</p>", // Un solo string en lugar de un array
-      slug: "experience-3"
     },
     {
       organization: "Web Developer",
       title: "Guacarí Hotel",
       dateRange: { start: "May 2024", end: "August 2024" },
-      description: "Developed a comprehensive management system for Guacari Hotel, utilizing React, Express, and PostgreSQL. Enhanced operational efficiency and customer satisfaction through features like real-time updates and automated reporting. Managed version control and streamlined deployment processes to ensure smooth and efficient updates.",
+      description: "Developed a real-time reservation management system using React, Express.js, and PostgreSQL, improving operational efficiency by 35%. Streamlined deployment processes, reducing deployment time by 30% through CI/CD pipeline integration and enabling 100% remote client access. Applied Git for version control, ensuring smooth collaboration, efficient branch management, and conflict resolution.",
       content: "<p>.</p>", // Un solo string en lugar de un array
-      slug: "experience-4"
+      slug: "experience-1"
+    },
+    {
+      organization: " Tutor in Machine Learning Technical Component",
+      title: "Sapiencia",
+      dateRange: { start: "September 2023", end: "November 2023" },
+      description: "Conducted Python-based Machine Learning courses for 30 students, achieving a 90% satisfaction rate and facilitating certification for all participants. Developed comprehensive course materials to bridge theoretical concepts with practical applications, enhancing the learning experience. Collaborated with a multidisciplinary team to optimize educational outcomes and maintain program excellence.",
+      content: "<p>.</p>",
+      slug: "experience-3"
+    },
+    {
+      organization: "Founder and CEO",
+      title: "Parchese Store",
+      dateRange: { start: "October 2017", end: "December 2023" },
+      description: "Spearheaded strategic initiatives, consistently meeting monthly sales targets and managing a portfolio of 2,500 products. Oversaw a team of five, fostering skills in teamwork, assertive communication, and high-pressure performance. Innovated inventory management and logistics solutions to streamline operations and enhance customer satisfaction. I led a team of 5, managing tasks such as payroll and inventory.",
+      content: "<p>.</p>", // Un solo string en lugar de un array
+      slug: "experience-2"
     },
     // Agrega más experiencias según sea necesario...
   ];
@@ -149,7 +260,7 @@ export default async function Home() {
   return (
     <DefaultLayout>
       <section className="h-[var(--h-main)] fcenter">
-        <div className="flex flex-col items-center space-y-2 sm:space-y-3 md:space-y-4 md:text-4xl">
+        <div className="flex flex-col items-center sm:space-y-3 md:space-y-4 md:text-4xl">
           <h1 className="text-[2rem] text-center tracking-normal sm:text-[2.5rem] md:text-[3.25rem] lg:text-[3.5rem] 2xl:text-[4rem] sm:tracking-wider">
             Hello, {"I'm "}
             <span className="font-bold primary-gradient">
@@ -157,7 +268,7 @@ export default async function Home() {
             </span>
           </h1>
           <div className="text-base tracking-wide text-center lg:pt-3 sm:text-xl md:text-2xl text-slate-300">
-            Full-Stack Web Developer and Student of Systems Engineer
+            Full-Stack Web Developer and Systems Engineering Student
           </div>
           <div className="text-sm italic font-bold tracking-wide text-center md:text-base lg:text-lg primary-gradient-2">
             Actively looking for full-time jobs positions!
@@ -179,7 +290,7 @@ export default async function Home() {
 
       {/* tech section */}
       <SectionContainer>
-        <SectionHeader>Technologies</SectionHeader>
+        <SectionHeader>Development Skills</SectionHeader>
         <div className="flex flex-wrap justify-center">
           <TechSection data={techItems} />
         </div>
--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const DOMAIN = "https://jeffbl.dev";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/juan-andres-rivera-4113642b8/";
 export const GITHUB_URL = "https://github.com/mono789";
 export const EMAIL_URL = "juan.rivera7@udea.edu.co";
-export const RESUME_URL = "/Juan Rivera.pdf";
+export const RESUME_URL = "/Juan Rivera cv.pdf";
 
 export const COLOR_TEXT = "rgb(239, 242, 250)";
 export const COLOR_BACKGROUND = "rgb(7, 9, 13)";
@@ -15,7 +15,7 @@ export const NICE_CUBIC = [0.19, 1, 0.22, 1];
 export const SANITY_CDN = "https://cdn.sanity.io";
 
 export const ABOUT_ME_TEXT = [
-  "I have experience in Python, C++, Java, JavaScript, and Kotlin, along with web development skills using CSS, HTML, MySQL, and Firebase. I’m organized, detail-oriented, and always looking to improve my skills. I adapt quickly to new technologies and enjoy taking on challenges, eager to contribute to any project I work on.",
+  "Full-Stack Developer with expertise in JavaScript, and Java. Proven experience in building and deploying innovative web solutions that improve system performance. Skilled in mentoring teams, fostering collaboration, and aligning technical efforts with business objectives to deliver impactful results.",
   "<span> I'm passionate about continuous learning and personal growth. I find motivation in solving problems and creating efficient solutions that make a difference.</span>",
   "<span class='primary-gradient-2' style='font-weight: 700'>I'm currently looking for full-time software development opportunities. If you're interested in working with me, feel free to reach out!</span>",
 ];
